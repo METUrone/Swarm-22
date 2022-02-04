99381abfdc3afba4aab61e2f9e79f759af799f4b
mertgungor <mmmertgungor@gmail.com>
2022-02-04 18:35:59 +0300

    swarm class and formation

---
+++ src/swarm/scripts/Swarm.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import math
+import time
+from Iris import Iris
+from threading import Thread
+from copy import deepcopy
+
+class Swarm:
+    def __init__(self,num_of_drones,vehicle):
+        self.agents = []
+        self.vehicle = vehicle
+        
+        if vehicle == "Iris":
+            for i in range(num_of_drones):
+                self.add_drone(i)
+
+        self.num_of_agents = len(self.agents)
+
+        simulation_origin_latitude = 47.3977419
+        simulation_origin_longitude = 8.5455935
+
+        for i in range(len(self.agents)):
+            pose = self.distance_of_drones(drone_a=self.agents[i], lat_b=simulation_origin_latitude, long_b=simulation_origin_longitude)
+            print(self.agents[i].id)
+            self.agents[i].set_starting_pose(pose[0], pose[1]) #Agent must be in the starting position (locally 0, 0) height does not matter
+            self.agents[i].get_global_pose()
+
+    def formation_coordinates(self, radius):
+        vector = [0,0]
+        vectors = []
+        for i in range(self.num_of_agents):
+            vectors.append(deepcopy(vector))
+
+        
+        vectors[0][0]=0
+        vectors[0][1]=radius
+
+        angle = (360/self.num_of_agents)*0.0174532925 #radians 
+        
+        for i in range(1,self.num_of_agents):
+            vectors[i][0] = vectors[i-1][0]*math.cos(angle) - vectors[i-1][1]*math.sin(angle)
+            vectors[i][1] = vectors[i-1][1]*math.cos(angle) + vectors[i-1][0]*math.sin(angle)
+
+        return vectors
+
+    def form_via_pose(self, radius):
+        coordinates = self.formation_coordinates(radius)
+        for i in range(len(self.agents)):
+            Thread(target=self.agents[i].move_global, args=(coordinates[i][0], coordinates[i][1], 5)).start()
+
+    
+
+    def add_drone(self,id):
+   
+        self.agents.append(Iris(id))
+
+    def swarm_square(self):
+        for i in range(len(self.agents)):
+            Thread(target = self.agents[i].draw_square, args=(5,)).start()
+
+    def distance_of_drones(self,drone_a, drone_b): # in meters
+
+        lat_b = drone_b.gps_pose_getter().latitude
+        long_b = drone_b.gps_pose_getter().longitude
+
+        return self.distance_of_drones(drone_a, lat_b, long_b)
+
+    def distance_of_drones(self, drone_a, lat_b, long_b): # in meters
+
+        lat_a = drone_a.gps_pose_getter().latitude
+        x_distance = 111139 * (lat_a - lat_b)
+        long_a = drone_a.gps_pose_getter().longitude
+        R = 63678.137
+
+        dlat = lat_b * math.pi / 180 - lat_a * math.pi/180 
+        dlon = long_b * math.pi / 180 - long_a * math.pi/180 
+        a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(lat_a*math.pi / 180) * math.cos(lat_b * math.pi / 180) * math.sin(dlon/2) * math.sin(dlon/2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+        d = R * c
+        abs_distance = d * 100
+
+        y_distance = math.sqrt(abs_distance**2-x_distance**2)
+
+        x_coefficient = 1
+        y_coefficient = 1
+
+        if(lat_a < lat_b):
+            x_coefficient = -1
+        if(long_a < long_b):
+            y_coefficient = -1
+
+        return x_distance*x_coefficient, y_distance*y_coefficient, abs_distance
+
+    def print_drones_pose(self):
+        j = 0
+        for i in self.agents:
+            print(j)
+            i.get_global_pose()
+            j += 1
+        print("------------")
+        time.sleep(1)
+
+    def return_starting_pose(self):
+        for i in self.agents:
+            Thread(target=i.move_local, args=(0,0,5)).start()
+
--- src/swarm/scripts/main.py
+++ src/swarm/scripts/main.py
@@ -1,102 +1,19 @@
 #!/usr/bin/env python3
 
 from Iris import Iris 
-from threading import Thread
-import math
+from Swarm import Swarm 
 import time
 
 
-drones = [] 
-
-def add_drone(id):
-    global drones
-    drones.append(Iris(id))
-
-def swarm_square(num_of_drones):
-    for i in range(num_of_drones):
-        Thread(target = drones[i].draw_square, args=(5,)).start()
-
-def distance_of_drones(drone_a, drone_b): # in meters
-
-    lat_a = drone_a.gps_pose_getter().latitude
-    lat_b = drone_b.gps_pose_getter().latitude
-    x_distance = 111139 * (lat_a - lat_b)
-    long_a = drone_a.gps_pose_getter().longitude
-    long_b = drone_b.gps_pose_getter().longitude
-    R = 63678.137
-
-    dlat = lat_b * math.pi / 180 - lat_a * math.pi/180 
-    dlon = long_b * math.pi / 180 - long_a * math.pi/180 
-    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(lat_a*math.pi / 180) * math.cos(lat_b * math.pi / 180) * math.sin(dlon/2) * math.sin(dlon/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = R * c
-    abs_distance = d * 100
-
-    y_distance = math.sqrt(abs_distance**2-x_distance**2)
-    #print(abs_distance)
-    #print(x_distance) 
-    #print(y_distance)
-    return x_distance, y_distance, abs_distance
-
-def distance_of_drones(drone_a, lat_b, long_b): # in meters
-
-    lat_a = drone_a.gps_pose_getter().latitude
-    x_distance = 111139 * (lat_a - lat_b)
-    long_a = drone_a.gps_pose_getter().longitude
-    R = 63678.137
-
-    dlat = lat_b * math.pi / 180 - lat_a * math.pi/180 
-    dlon = long_b * math.pi / 180 - long_a * math.pi/180 
-    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(lat_a*math.pi / 180) * math.cos(lat_b * math.pi / 180) * math.sin(dlon/2) * math.sin(dlon/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = R * c
-    abs_distance = d * 100
-
-    y_distance = math.sqrt(abs_distance**2-x_distance**2)
-
-    x_coefficient = 1
-    y_coefficient = 1
-
-    if(lat_a < lat_b):
-        x_coefficient = -1
-    if(long_a < long_b):
-        y_coefficient = -1
-
-    return x_distance*x_coefficient, y_distance*y_coefficient, abs_distance
-
-def prin_drones_pose(drones):
-    j = 0
-    for i in drones:
-        print(j)
-        i.get_global_pose()
-        j += 1
-    print("------------")
-    time.sleep(1)
-    
-def swarm_go_starting_point(drones):
-    for i in drones:
-        i.move_local(0,0,5)
-
 if __name__ == "__main__":
     
-    num_of_drones = 3
-    simulation_origin_latitude = 47.3977419
-    simulation_origin_longitude = 8.5455935
+    num_of_agents = 3
+    vehicle = "Iris"
 
-    for i in range(num_of_drones):
-        add_drone(i)
-        pose = distance_of_drones(drones[i], simulation_origin_latitude, simulation_origin_longitude)
-        print(pose[0], pose[1])
-        drones[i].set_starting_pose(pose[0], pose[1]) #Agent must be in the starting position (locally 0, 0) height does not matter
-        drones[i].get_global_pose()
-
-    print("-----------\n")
-
-    #swarm_square(num_of_drones)
+    swarm = Swarm(num_of_agents, vehicle)
     
-    #swarm_go_starting_point(drones)
-    drones[1].velocity_command(0)
-    time.sleep(5)
-    
-    
-    
+    swarm.form_via_pose(num_of_agents)
+    time.sleep(3)
+    swarm.swarm_square()
+    time.sleep(15)
+    swarm.return_starting_pose()
