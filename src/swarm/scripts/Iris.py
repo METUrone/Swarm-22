e4b831067944de50adc223f4d82bbfd6b02b3428
mertgungor <mmmertgungor@gmail.com>
2022-02-24 00:08:59 +0300

    global_pose changed to position

--- src/swarm/scripts/Iris.py
+++ src/swarm/scripts/Iris.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
+import numpy as np
 
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import NavSatFix
@@ -26,10 +27,10 @@ class Iris:
         self.pose_sub = rospy.Subscriber("/uav{}/mavros/global_position/global".format(self.id), NavSatFix, self.current_gps_pose_callback)
         self.odometry_sub = rospy.Subscriber("/uav{}/mavros/global_position/local".format(self.id), Odometry, self.current_odometry_pose_callback)
 
-        self.global_pose = Odometry()
-        self.global_pose.pose.pose.position.x = self.starting_x + self.odomery_pose.pose.pose.position.x 
-        self.global_pose.pose.pose.position.y = self.starting_y + self.odomery_pose.pose.pose.position.y 
-        self.global_pose.pose.pose.position.z = self.odomery_pose.pose.pose.position.z
+        self.global_pose = np.array([0,0,0]) #x, y, z
+        self.global_pose[0] = self.starting_x + self.odomery_pose.pose.pose.position.x 
+        self.global_pose[1] = self.starting_y + self.odomery_pose.pose.pose.position.y 
+        self.global_pose[2] = self.odomery_pose.pose.pose.position.z
 
         print("Iris{} waiting...".format(self.id))
         
@@ -46,9 +47,9 @@ class Iris:
     def current_odometry_pose_callback(self, data):
         
         self.odomery_pose = deepcopy(data)
-        self.global_pose.pose.pose.position.x = self.starting_x + self.odomery_pose.pose.pose.position.x
-        self.global_pose.pose.pose.position.y = self.starting_y + self.odomery_pose.pose.pose.position.y 
-        self.global_pose.pose.pose.position.z = self.odomery_pose.pose.pose.position.z
+        self.global_pose[0] = self.starting_x + self.odomery_pose.pose.pose.position.x
+        self.global_pose[1] = self.starting_y + self.odomery_pose.pose.pose.position.y 
+        self.global_pose[2] = self.odomery_pose.pose.pose.position.z
         
 
     def gps_pose_getter(self):
@@ -57,11 +58,11 @@ class Iris:
     def set_starting_pose(self, x, y):
         self.starting_x = x
         self.starting_y = y
-        self.global_pose.pose.pose.position.x = self.starting_x + self.odomery_pose.pose.pose.position.x 
-        self.global_pose.pose.pose.position.y = self.starting_y + self.odomery_pose.pose.pose.position.y 
-        self.global_pose.pose.pose.position.z = self.odomery_pose.pose.pose.position.z
+        self.global_pose[0] = self.starting_x + self.odomery_pose.pose.pose.position.x 
+        self.global_pose[1] = self.starting_y + self.odomery_pose.pose.pose.position.y 
+        self.global_pose[2] = self.odomery_pose.pose.pose.position.z
 
-    def get_global_pose(self):
+    def position(self):
         #print("x: {}\ny: {}\n".format(self.global_pose.pose.pose.position.x, self.global_pose.pose.pose.position.y))
         return self.global_pose
 
