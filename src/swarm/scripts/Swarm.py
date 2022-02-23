b2f65bcff260def9116d4608de63bcf9ea79d38b
mertgungor <mmmertgungor@gmail.com>
2022-02-24 00:04:18 +0300

    potential field change

--- src/swarm/scripts/Swarm.py
+++ src/swarm/scripts/Swarm.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-
-from cmath import sqrt
+import numpy as np
 import math
-from re import S
 import time
 from Iris import Iris
 from threading import Thread
 from copy import deepcopy
+
+from TurtleBot import TurtleBot
 
 class Swarm:
     def __init__(self,num_of_drones,vehicle):
@@ -16,25 +16,25 @@ class Swarm:
         if vehicle == "Iris":
             for i in range(num_of_drones):
                 self.add_drone(i)
+        elif vehicle == "TurtleBot":
+            for i in range(num_of_drones):
+                self.add_turtlebot(i)
 
         self.num_of_agents = len(self.agents)
 
         simulation_origin_latitude = 47.3977419
         simulation_origin_longitude = 8.5455935
 
-        for i in range(len(self.agents)):
-            pose = self.distance_to_pose(drone_a=self.agents[i], lat_b=simulation_origin_latitude, long_b=simulation_origin_longitude)
-            print(self.agents[i].id)
-            self.agents[i].set_starting_pose(pose[0], pose[1]) #Agent must be in the starting position (locally 0, 0) height does not matter
-            self.agents[i].get_global_pose()
+        if self.vehicle != "TurtleBot":
+            for i in range(len(self.agents)):
+                pose = self.distance_to_pose(drone_a=self.agents[i], lat_b=simulation_origin_latitude, long_b=simulation_origin_longitude)
+                print(self.agents[i].id)
+                self.agents[i].set_starting_pose(pose[0], pose[1]) #Agent must be in the starting position (locally 0, 0) height does not matter
+                self.agents[i].position()
 
     def formation_coordinates(self, radius):
-        vector = [0,0]
-        vectors = []
-        for i in range(self.num_of_agents):
-            vectors.append(deepcopy(vector))
+        vectors = np.zeros(shape=(self.num_of_agents,2))
 
-        
         vectors[0][0]=0
         vectors[0][1]=radius
 
@@ -46,6 +46,29 @@ class Swarm:
 
         return vectors
 
+    def attractive_force(self, id, target_pose, attractive_constant = 0.3):
+        attractive_force_x = (target_pose[0] - self.agents[id].position()[0])*attractive_constant
+        attractive_force_y = (target_pose[1] - self.agents[id].position()[1])*attractive_constant
+        return attractive_force_x, attractive_force_y
+
+    def repulsive_force(self,id, repulsive_constant =-17,repulsive_threshold = 2): 
+        repulsive_force_x = 0
+        repulsive_force_y = 0
+
+        for i in range(len(self.agents)):
+                if i == id:
+                    continue
+                y_distance = self.agents[id].position()[1] - self.agents[i].position()[1]
+                x_distance = self.agents[id].position()[0] - self.agents[i].position()[0]
+
+                d = ((y_distance**2) + (x_distance**2))**(1/2)
+
+                if d < repulsive_threshold and y_distance != 0 and x_distance != 0:
+                    repulsive_force_y += (1/(y_distance**2))*(1/repulsive_threshold - 1/y_distance)*repulsive_constant
+                    repulsive_force_x += (1/(x_distance**2))*(1/repulsive_threshold - 1/x_distance)*repulsive_constant
+
+        return repulsive_force_x, repulsive_force_y
+
     def form_via_pose(self, radius):
         coordinates = self.formation_coordinates(radius)
         for i in range(len(self.agents)):
@@ -53,41 +76,28 @@ class Swarm:
 
     def single_potential_field(self, radius, id):
         coordinates = self.formation_coordinates(radius)
-        attractive_constant = 0.3
-        repulsive_constant =-17 #-8
-        repulsive_force_x = 0
-        repulsive_force_y = 0
-        repulsive_threshold = 2 #3
 
-        for _ in range(2000):
+        for _ in range(4000):
             repulsive_force_x = 0
             repulsive_force_y = 0
 
-            attractive_force_x = (coordinates[id][0] - self.agents[id].get_global_pose().pose.pose.position.x)*attractive_constant
-            attractive_force_y = (coordinates[id][1] - self.agents[id].get_global_pose().pose.pose.position.y)*attractive_constant
-
-            for i in range(len(self.agents)):
-                if i == id:
-                    continue
-                a = self.agents[id].get_global_pose().pose.pose.position.y - self.agents[i].get_global_pose().pose.pose.position.y
-                b = self.agents[id].get_global_pose().pose.pose.position.x - self.agents[i].get_global_pose().pose.pose.position.x
-
-                d = ((a**2) + (b**2))**(1/2)
-
-                if d < repulsive_threshold:
-                    repulsive_force_y += (1/(a**2))*(1/repulsive_threshold - 1/a)*repulsive_constant
-                    repulsive_force_x += (1/(b**2))*(1/repulsive_threshold - 1/b)*repulsive_constant
+            attractive_force_x, attractive_force_y = self.attractive_force(target_pose=coordinates[id], id=id)
+            repulsive_force_x, repulsive_force_y = self.repulsive_force(id=id)
 
 
             vel_x = attractive_force_x + repulsive_force_x
             vel_y = attractive_force_y + repulsive_force_y
-            self.agents[id].velocity_command(vel_x, vel_y)
+            
+            self.agents[id].velocity_command(linear_x=vel_x, linear_y=vel_y)
+
             time.sleep(0.001)
 
-        #self.agents[id].move_global(coordinates[id][0], coordinates[id][1], 5)
+        if self.vehicle == "Iris":
+            self.agents[id].move_global(coordinates[id][0], coordinates[id][1], 5) # makes more stable
         print("done")
 
     def form_via_potential_field(self, radius):
+        print(self.formation_coordinates(radius))
         for i in range(len(self.agents)):
             Thread(target=self.single_potential_field, args = (radius,i)).start()
 
@@ -95,6 +105,10 @@ class Swarm:
     def add_drone(self,id):
    
         self.agents.append(Iris(id))
+
+    def add_turtlebot(self,id):
+   
+        self.agents.append(TurtleBot(id))
 
     def swarm_square(self):
         for i in range(len(self.agents)):
@@ -137,7 +151,7 @@ class Swarm:
         j = 0
         for i in self.agents:
             print(j)
-            i.get_global_pose()
+            i.position()
             j += 1
         print("------------")
         time.sleep(1)
--- src/swarm/scripts/main.py
+++ src/swarm/scripts/main.py
@@ -1,30 +1,18 @@
 #!/usr/bin/env python3
 
+from threading import Thread
 from Swarm import Swarm 
 import time
 import sys
 import rospy
+from TurtleBot import TurtleBot
+
+
 
 if __name__ == "__main__":
-    
-    num_of_agents = 5
+
+
     vehicle = "Iris"
-
-    swarm = Swarm(num_of_agents, vehicle)
-
-    try:
-
-        if sys.argv[1]=="1":
-        
-            #swarm.form_via_pose(4)
-            swarm.form_via_potential_field(4)
-            #swarm.single_potential_field(3, 2)
-            print("square")
-            time.sleep(10)
-            #swarm.swarm_square()
-            #time.sleep(15)
-        else:
-            swarm.return_starting_pose()
-
-    except rospy.exceptions.ROSInterruptException:
-        print("shutdown")
+    swarm = Swarm(3, vehicle)
+    #swarm.form_via_potential_field(4)
+    swarm.return_starting_pose()
